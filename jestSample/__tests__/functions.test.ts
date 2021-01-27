06bf5e5a46bafc61474234691ea40aa3768c9679
natsume0718 <moto718program@gmail.com>
2021-01-27 20:50:42 +0900

    sumOfArray test

--- jestSample/__tests__/functions.test.ts
+++ jestSample/__tests__/functions.test.ts
@@ -1,1 +1,19 @@
 // todo: ここに単体テストを書いてみましょう！
+import { sumOfArray } from "../functions";
+
+describe("sumOfArray", (): void => {
+  test(`sumOfArray()は正常に計算を行います`, (): void => {
+    expect(sumOfArray([1, 1])).toBe(2);
+  });
+  test(`sumOfArray()は正常に計算を行います`, (): void => {
+    expect(sumOfArray([1, 2, 3])).toBe(6);
+  });
+  test(`エラー`, (): void => {
+    expect((): void => {
+      sumOfArray([]);
+    }).toThrow();
+  });
+  // test(`コンパイルエラー`, (): void => {
+  //   expect(sumOfArray(["a", "b", "c"])).toBe("abc");
+  // });
+});
